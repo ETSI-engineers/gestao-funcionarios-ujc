b693ccdf3f84dfe9a50cea26fd8d2e891f5ffd01
helderjosue <helderjosuemata@gmail.com>
2021-06-30 19:12:16 +0200

    ADDING FORMS

---
+++ src/main/java/mz/ac/ujc/esa/boot/controller/web/CargoController.java
@@ -0,0 +1,35 @@
+package mz.ac.ujc.esa.boot.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cargos")
+public class CargoController {
+
+	@GetMapping("cadastrar")
+	public String cadastrar() {
+
+		return "/admin/pages/cargos/add-cargo";
+	}
+
+	@GetMapping("listar")
+	public String listar() {
+
+		return "/admin/pages/cargos/list-cargo";
+	}
+
+	@GetMapping("actualizar")
+	public String actualizar() {
+
+		return "/admin/pages/cargos/add-cargo";
+	}
+
+	@GetMapping("apagar")
+	public String apagar() {
+
+		return "/admin/pages/cargos/add-cargo";
+	}
+
+}
---
+++ src/main/java/mz/ac/ujc/esa/boot/controller/web/FuncionarioController.java
@@ -0,0 +1,35 @@
+package mz.ac.ujc.esa.boot.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/funcionarios")
+public class FuncionarioController {
+
+    @GetMapping("cadastrar")
+    public String cadastrar() {
+
+        return "/admin/pages/funcionarios/add-func";
+    }
+
+    @GetMapping("listar")
+    public String listar() {
+
+        return "/admin/pages/funcionarios/list-func";
+    }
+
+    @GetMapping("actualizar")
+    public String actualizar() {
+
+        return "/admin/pages/funcionarios/list-curso";
+    }
+
+    @GetMapping("apagar")
+    public String apagar() {
+
+        return "/admin/pages/funcionarios/list-curso";
+    }
+
+}
